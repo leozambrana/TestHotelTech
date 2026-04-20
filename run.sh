34606973e8f6ce2bf8dbc0e27c2f1ff6610e8952
leonardozambrana1@gmail.com <leonardozambrana1@gmail.com>
2026-04-20 20:07:15 -0300

    remove sh run script

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Este script executa o comando npm start na raiz, 
-# repassando todos os argumentos para o processo do backend.
-# Exemplo: ./run.sh --map=map.ascii --bookings=bookings.json
-
-npm start "$@"
